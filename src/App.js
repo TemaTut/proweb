157be3a5d944dabf1c1432a6994fd5b18e59db88
Tematut <0009494@mail.ru>
2023-07-09 21:22:03 +0400

    feat: react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import "./styles/App.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+// import MySelect from "./components/UI/select/MySelect";
+// import MyButton from "./components/UI/button/MyButton";
+// import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "JS 1", body: "Description" },
-    { id: 2, title: "JS 2", body: "Description" },
-    { id: 3, title: "JS 3", body: "Description" },
+    { id: 1, title: "aa", body: "ss" },
+    { id: 2, title: "bb", body: "dd" },
+    { id: 3, title: "ff", body: "ee" },
   ]);
+
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false)
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false)
   };
 
   const removePost = (post) => {
@@ -20,19 +45,20 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 20 }} onClick={() => setModal(true)} >
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
-      <div>
-        <select>
-          <option value="value1">По названию</option>
-          <option value="value1">По описанию</option>
-        </select>
-      </div>
-      {posts.length ? (
-        <PostList remove={removePost} posts={posts} title="Посты про js" />
-      ) : (
-        <h2 style={{ textAlign: "center" }}>Нет задач!</h2>
-      )}
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Посты про js"
+      />
     </div>
   );
 }
